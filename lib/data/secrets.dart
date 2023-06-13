173cd8bd3143ec69350e1bbc0da915f143660ee1
Avadhkumar-geek <avadhkachhadiya@gmail.com>
2023-06-13 19:37:05 +0530

    some minor fixes

---
+++ lib/data/secrets.dart
@@ -0,0 +1,9 @@
+String? devApiKey = 'oYTplqgggUrLTPp-4-ocyo-hn093sIBgDhls5o9_8ok'; //'fg-OPO5D3V8UKGSJI9A9PY85TD9XFLWCM314PDNU511';
+
+// wiredash secrets
+// Create account in https://wiredash.io/ to use its Feedback SDK
+const String label1 = 'label-jmr4ih3qig';
+const String label2 = 'label-6icfkrtiil';
+const String label13 = 'label-jyi00inv6e';
+const String projectId = 'studentai-4joyq7b';
+const String secretKey = 'R9Q1KSg6aUbRpkB-l4S_mnmUDRD6V2md';
